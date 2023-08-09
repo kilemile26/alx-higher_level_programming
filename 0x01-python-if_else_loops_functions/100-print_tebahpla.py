0e1fce36b6b87ccd681d5cd66b0912c0f5f371a7
kilemile26 <harunakilemile@gmail.com>
2023-08-09 11:46:52 -0700

    4&14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for char_code in range(122, 96, -1):
-    print("{}".format(chr(char_code).swapcase()), end="")
+    if char_code % 2 == 0:
+        print("{}".format(chr(char_code)), end="")
+    else:
+        print("{}".format(chr(char_code - 32)), end="")
