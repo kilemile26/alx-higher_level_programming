82a316ecbc860ceef2b2ca83985e057b4607acb3
kilemile26 <harunakilemile@gmail.com>
2023-08-09 12:30:48 -0700

    print nums1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end="")
-        if tens_digit != 8 or ones_digit != 9:
+for number in range(10):
+    for num in range(number + 1, 10):
+        print("{:d}{:d}".format(number, num), end="")
+        if number != 8 or num != 9:
             print(", ", end="")
 print()
