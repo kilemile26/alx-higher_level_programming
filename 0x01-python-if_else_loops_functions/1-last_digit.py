39c4c42befa5948978d998aaec14e90ada638cd4
kilemile26 <harunakilemile@gmail.com>
2023-08-09 11:18:14 -0700

    print asci

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number*-1) % 10*-1
+
+if number >=0:
+    last = abs(number) % 10
+else:
+    last = (abs(number*-1) % 10*)-1
 output = "Last digit of {} is {} and is".format(number, last)
 if last > 5:
     print(output + " greater than 5")
