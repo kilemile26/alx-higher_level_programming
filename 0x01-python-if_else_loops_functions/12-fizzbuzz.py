ed263ec14054ff911807f47668e44882880d30a4
kilemile26 <harunakilemile@gmail.com>
2023-08-10 10:27:07 -0700

    3 fibuzz1

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    result = ""
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             result += "FizzBuzz "
-#            #print("FizzBuzz ", end="")
         elif number % 5 == 0:
             result += "Buzz "
-#            #print("Buzz ", end="")
         elif number % 3 == 0:
             result += "Fizz "
-#            #print("Fizz ", end="")
         else:
-            result += str(number) + " "
-#            #print(number, "", end="")
+            print(number, "", end="")
 
 
 fizzbuzz()
