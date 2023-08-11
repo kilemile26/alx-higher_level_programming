7d337cf62a2705ab071ce9b707fd69a90eb35a6f
kilemile26 <harunakilemile@gmail.com>
2023-08-11 09:57:31 -0700

    args_addition1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,7 @@ if __name__ == "__main__":
     import sys
 
 
-    total = sum(int(arg) for arg in sys.arg[1:])
-    print("{}".format(total))
+    total = 0
+    for arg in sys.arg[1:]):
+        total += int(arg)
+    print(total)
