2f249270fa89ad5a6d55a1147e6e68c32872f8bb
kilemile26 <harunakilemile@gmail.com>
2023-08-11 10:10:56 -0700

    arg

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
 
 
     arg_tot = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            arg_tot += int(arg)
+    for arg in sys.argv[1:]:
+        arg_tot += int(arg)
     print(arg_tot)
