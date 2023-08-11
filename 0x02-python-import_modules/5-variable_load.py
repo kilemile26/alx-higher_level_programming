11838bb9f59b8f249785dcb8ff0b968995b21c04
kilemile26 <harunakilemile@gmail.com>
2023-08-11 10:22:07 -0700

    importing variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4)
-    filtered_names = filter(lambda name: not name.startswith("__"), names)
-    for name in filtered_names:
-        print(name)
+    n = dir(hidden_4)
+    for name in n:
+        if not name.startswith("__"):
+            print(name)
