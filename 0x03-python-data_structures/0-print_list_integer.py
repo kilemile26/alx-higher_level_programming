9518b8f1d305ec75bf2db0c1476fb307d414a57b
kilemile26 <harunakilemile@gmail.com>
2023-08-16 06:52:45 -0700

    vvv

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,3 +2,8 @@
 if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         print("{:d}".format(my_list[n]) for n in my_list)
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
