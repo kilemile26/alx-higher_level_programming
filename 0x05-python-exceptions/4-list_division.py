6bcd62c944351de54cf0b50a18ede7e3bd23a6e4
kilemile26 <harunakilemile@gmail.com>
2023-08-28 11:20:39 -0700

    lists division1

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,21 +5,20 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-
             if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
-                if my_list_2[i] == 0:
-                    result.append(0)
-                    print("division by 0")
-                else:
-                    result.append(my_list_1[i] / my_list_2[i])
+                result.append(my_list_1[i] / my_list_2[i])  # Perform the division
             else:
-                result.append(0)
+                result.append(0)  # Handle wrong type
                 print("wrong type")
+        except ZeroDivisionError:
+            result.append(0)  # Handle division by 0
+            print("division by 0")
         except IndexError:
-            result.append(0)
+            result.append(0)  # Handle out of range
             print("out of range")
         finally:
-            pass
+            # This code will be executed regardless of whether an exception occurred or not
+            pass  # You can add specific actions here if needed
 
+    return result
 
-return result
