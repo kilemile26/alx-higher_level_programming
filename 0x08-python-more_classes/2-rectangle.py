0fb413a46545de1ae82541022299ce6d862312b7
kilemile26 <harunakilemile@gmail.com>
2023-09-28 13:51:58 -0700

    corr4

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -102,5 +102,4 @@ class Rectangle:
     Returns:
         str: The formatted string representation.
     """
-    return "{} - {} => {} / {}".format(
-            self.__width, self.__height, self.area(), self.perimeter())
+    return "{} - {} => {} / {}".format(self.__width, self.__height, self.area(), self.perimeter())
