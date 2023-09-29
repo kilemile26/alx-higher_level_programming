23806fec5417788375894ae826b6aeca9b63f57d
kilemile26 <harunakilemile@gmail.com>
2023-09-29 11:25:18 -0700

    lock1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -32,4 +32,4 @@ class LockedClass:
         if name == "first_name":
             self.__dict__[name] = value
         else:
-            raise AttributeError("'LockedClass' object has no attribute ''".format(name))
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
