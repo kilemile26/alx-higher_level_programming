57bd3a894ef0774933811f90dcfd3ae363aadab6
kilemile26 <harunakilemile@gmail.com>
2023-10-02 08:27:27 -0700

    8-rect

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -32,33 +32,16 @@ class BaseGeometry:
         return value
 
 
-class BaseRectangle(BaseGeometry):
-    """A base class for rectangles."""
-
-    def __init__(self, width, height):
-        """Initialize a BaseRectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Compute the area of the rectangle."""
-        return self.__width * self.__height
-
-
 class Rectangle(BaseGeometry):
     """A class representing a rectangle."""
 
-    def __str__(self):
-        """Return a string representation of the rectangle."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    def __init__(self, width, height):
+        """Initialisation function for the Rectangle class"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
 
 if __name__ == "__main__":
     r = Rectangle(3, 5)
     print(r)
+    print(dir(r))
