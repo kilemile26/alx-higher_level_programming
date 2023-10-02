f44b8dd48ba69aab742d792ca19b5e7d9752e945
kilemile26 <harunakilemile@gmail.com>
2023-10-02 08:48:12 -0700

    advanced2

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines a custom integer class with 
+This module defines a custom integer class with
 inverted '==' and '!=' operators.
 """
 
@@ -8,14 +8,17 @@ inverted '==' and '!=' operators.
 class MyInt(int):
     """
     A custom integer class that inherits from the built-in int class.
-    This class inverts the behavior of the equality (==) and inequality (!=) operators.
+    This class inverts the behavior of the equality (==) and 
+    inequality (!=) operators.
 
     Attributes:
         No additional attributes.
 
     Methods:
-        __eq__(self, other): Inverts the behavior of the equality operator (==).
-        __ne__(self, other): Inverts the behavior of the inequality operator (!=).
+        __eq__(self, other): Inverts the behavior of the 
+        equality operator (==).
+        __ne__(self, other): Inverts the behavior of the 
+        inequality operator (!=).
     """
     def __eq__(self, other):
         """
