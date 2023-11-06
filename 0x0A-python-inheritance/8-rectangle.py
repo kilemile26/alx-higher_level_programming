ad7b0ca917567ad9b3643df5ce8551992f7ff257
kilemile26 <harunakilemile@example.com>
2023-11-06 08:47:50 -0800

    inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module for Rectangle class"""
+
+
+class BaseGeometry:
+    """A base class for geometric shapes."""
+
+    def area(self):
+        """Compute the area of the geometric shape.
+
+        Raises:
+            Exception: This method should be implemented in derived classes.
+
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that a value is a positive integer.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not a positive integer.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialisation function for the Rectangle class"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+    print(r)
+    print(dir(r))
+
+    try:
+        print("Rectangle: {} - {}".format(r.width, r.height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
