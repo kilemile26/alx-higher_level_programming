f0b10dd43797c51dc328030c053136c037e9d431
kilemile26 <harunakilemile@example.com>
2023-11-06 13:39:32 +0000

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,33 @@
+ass MyList that inherits from list"""
+
+
+class MyList(list):
+    """
+    A custom list class that inherits from the built-in list class.
+    """
+
+    def print_sorted(self):
+        """
+        Print the list in ascending order without modifying the original list.
+
+        Returns:
+            None
+        """
+        print("{}".format(sorted(self)))
+
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(3)
+    my_list.append(1)
+    my_list.append(2)
+    my_list.print_sorted()  # Should print [1, 2, 3]
+    my_list.append(3)
+    my_list.append(-1)
+    my_list.append(2)
+    my_list.print_sorted()  # Should print [-1, 1, 2, 2, 3, 3]
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(-2)
+    my_list.print_sorted()  # Should print [-2, -1, 1, 1, 2, 2, 3, 3, 4]
+    print(my_list)  # Should print [-2, -1, 1, 1, 2, 2, 3, 3, 4]"
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+all about inheritance with python
