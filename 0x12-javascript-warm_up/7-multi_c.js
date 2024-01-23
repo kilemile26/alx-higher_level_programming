0a773bb17d6422dcb9ed8875c12353aa32126463
kilemile26 <harunakilemile@example.com>
2024-01-23 01:48:17 -0800

    7corect

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+/*
 const occurrences = parseInt(process.argv[2]);
 
 if (!isNaN(occurrences) && occurrences > 0) {
@@ -10,4 +10,15 @@ if (!isNaN(occurrences) && occurrences > 0) {
   ;
 } else {
   console.log('Missing number of occurrences');
+}
+*/
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
