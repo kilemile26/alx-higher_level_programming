d8f79ca1e8483230a24116b9370404453e29df50
kilemile26 <harunakilemile@gmail.com>
2024-01-26 15:59:03 +0000

    trial 1

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,3 +1,4 @@
+/*
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
@@ -5,9 +6,11 @@ class Rectangle {
       // If w or h is equal to 0 or not a positive integer, create an empty object
       this.width = w;
       this.height = h;
+    } else {
+      return {};
     }
-  print ()
-  {
+  }
+  print () {
     // Instance method to print the rectangle using the character X
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
@@ -16,3 +19,27 @@ class Rectangle {
 }
 
 module.exports = Rectangle;
+*/
+
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      this.width = w;
+      this.height = h;
+    } else {
+      return {}; // You might want to handle this case differently
+    }
+  }
+
+  print() {
+    // Instance method to print the rectangle using the character X
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
+
