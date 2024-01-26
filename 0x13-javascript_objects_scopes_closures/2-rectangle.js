c850bb97c4a0ebbe00aed21190da15998b28588b
kilemile26 <harunakilemile@gmail.com>
2024-01-26 13:12:25 +0000

    obj1.

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,11 @@ class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
       // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
+      return Rectangle{};
     }
     this.width = w;
     this.height = h;
-    }
+  }
 }
 
 module.exports = Rectangle;
