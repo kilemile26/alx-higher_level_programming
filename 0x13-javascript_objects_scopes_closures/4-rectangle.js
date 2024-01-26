5357804db17bdbdebf48826cfa4a327e958ad984
kilemile26 <harunakilemile@gmail.com>
2024-01-26 16:29:16 +0000

    trial 4..

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,27 +7,27 @@ class Rectangle {
     }
     this.width = w;
     this.height = h;
-}
-
-print () {
-  // Instance method to print the rectangle using the character 'X'
-  for (let i = 0; i < this.height; i++) {
-    console.log('X'.repeat(this.width));
   }
-}
 
-rotate () {
-  // Instance method to exchange the width and height of the rectangle
-  const temp = this.width;
-  this.width = this.height;
-  this.height = temp;
-}
+  print () {
+    // Instance method to print the rectangle using the character 'X'
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 
-double () {
-  // Instance method to double the width and height of the rectangle
-  this.width *= 2;
-  this.height *= 2;
-}
+  rotate () {
+    // Instance method to exchange the width and height of the rectangle
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    // Instance method to double the width and height of the rectangle
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
