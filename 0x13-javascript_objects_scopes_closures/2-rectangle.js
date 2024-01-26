cd7c482a68ea83e77e3999b7e44a58d8a165c211
kilemile26 <harunakilemile@gmail.com>
2024-01-26 13:23:50 +0000

    obj2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+    if (w <= 0 || h <= 0) {
       // If w or h is equal to 0 or not a positive integer, create an empty object
     this.width = w;
     this.height = h;
