9d31a74647254c41726180ad0b5dc176b08c3a2e
kilemile26 <harunakilemile@gmail.com>
2024-01-26 15:54:23 +0000

    dd

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,7 +3,6 @@ class Rectangle {
   constructor(w, h) {
     if (w > 0 && h > 0) {
       // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
       this.width = w;
       this.height = h;
     }
