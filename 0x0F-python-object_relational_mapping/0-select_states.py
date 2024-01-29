bc07d7c05c054a6212b1e5397382e3ff48c0fded
kilemile26 <harunakilemile@gmail.com>
2024-01-29 19:39:30 +0000

    list all0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,9 +4,9 @@ import sys
 
 if __name__ == "__main__":
     # Check if three arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+    # if len(sys.argv) != 4:
+       # print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+       # sys.exit(1)
 
     # Get credentials from command line arguments
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
