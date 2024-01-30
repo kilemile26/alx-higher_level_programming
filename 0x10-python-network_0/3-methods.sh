ba3cceaa8c27826157da50edb63c61e7e9837efe
kilemile26 <harunakilemile@gmail.com>
2024-01-30 17:21:57 +0000

    4.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | awk '/Allow/ {print substr($0, index($0,$2))}'
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
