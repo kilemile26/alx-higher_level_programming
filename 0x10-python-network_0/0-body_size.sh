4f18933ded0f5a1107620a0fbb2ca7f3ec724152
kilemile26 <harunakilemile@gmail.com>
2024-01-30 17:11:07 +0000

    3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to get the response body and measure its size in bytes
-curl -s "$1" | wc -c
+curl -s "$1" | grep -i Content-Length | awk '{print $2}'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to send a GET request and display the body if the status code is 200
-curl -s -w "%{http_code}" "$1"
+curl -sL "$1"
