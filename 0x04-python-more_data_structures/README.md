d067868151f075ed1d6523f8da916f434eea6022
kilemile26 <harunakilemile@gmail.com>
2025-07-22 22:02:33 +0200

    t3

--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -149,4 +149,3 @@ Repo:
 GitHub repository: alx-higher_level_programming
 Directory: 0x04-python-more_data_structures
 File: 3-common_elements.py
-
--- 0x04-python-more_data_structures/testing.py
+++ 0x04-python-more_data_structures/testing.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3$
-nums = [1, 2, 3]$
-squared = list(map(lambda x: x**2, nums))$
-print(squared)$
+#!/usr/bin/env python3
+nums = [1, 2, 3]
+squared = list(map(lambda x: x**2, nums))
+print(squared)
