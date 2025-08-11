32f02e0a562d88d578e7ee18ac4189bf4730dc3d
kilemile26 <harunakilemile@gmail.com>
2025-08-11 21:17:37 +0200

    t0.1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
 # This code squares each element in a matrix using nested map functions.
-# The outer map applies to each row, and the inner map applies to each element in the row.
+# The outer map applies to each row, and the inner map applies to each element
