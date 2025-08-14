2725e4727a3d0449d2d3fb787c68f0d98d09135a
kilemile26 <harunakilemile@gmail.com>
2025-08-14 18:35:29 +0200

    t100.2

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    return (sum(x["score"] * x["weight"] for x in my_list) / sum(x["weight"] for x in my_list)) if my_list else 0
+    return (sum(x[0] * x[1] for x in my_list) / sum(x[1] for x in my_list)) if my_list else 0
