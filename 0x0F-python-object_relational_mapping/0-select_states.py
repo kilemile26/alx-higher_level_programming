5c67b206a552bb859aea27f3680864e62f1f2d5e
kilemile26 <harunakilemile@gmail.com>
2025-11-11 11:35:10 +0200

    T1&21

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-     #Check if three arguments are provided
-     if len(sys.argv) != 4:
+    # Check if three arguments are provided
+    if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
@@ -12,27 +12,21 @@ if __name__ == "__main__":
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-    # Create a cursor object
-    cursor = db.cursor()
-nano ~/.git-credentials
-    # Execute the SQL query to select all states, ordered by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+# Create a cursor object
+cursor = db.cursor()
 
-    # Fetch all the rows
-    rows = cursor.fetchall()
+# Execute the SQL query to select all states, ordered by id
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Print the results
-    for row in rows:
-        print(row)
+# Fetch all the rows
+rows = cursor.fetchall()
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+# Print the results
+for row in rows:
+    print(row)
+
+# Close the cursor and database connection
+cursor.close()
+db.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,8 +3,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-     #Check if three arguments are provided
-     if len(sys.argv) != 4:
+    # Check if three arguments are provided
+    if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
@@ -12,13 +12,7 @@ if __name__ == "__main__":
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database,)
 
     # Create a cursor object
     cursor = db.cursor()
